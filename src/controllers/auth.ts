04873c1ecc768f8fdfdf1a4481204583f4c35d2d
1quzary <quzaryy@gmail.com>
2025-06-17 20:21:33 +0300

    login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
 import { prismaCilent } from '..'
-import { hashSync } from 'bcrypt'
+import { hashSync, compareSync } from 'bcrypt'
+import * as jwt from 'jsonwebtoken'
+import { JWT_SECRET } from '../secrets'
 
 export const signup = async (req: Request, res: Response) => {
 	const { email, password, name } = req.body
@@ -17,4 +19,23 @@ export const signup = async (req: Request, res: Response) => {
 		},
 	})
 	res.json(user)
+}
+
+export const login = async (req: Request, res: Response) => {
+	const { email, password } = req.body
+
+	let user = await prismaCilent.user.findFirst({ where: { email } })
+	if (!user) {
+		throw Error('User does not exists!')
+	}
+	if (!compareSync(password, user.password)) {
+		throw Error('Incorrect password!')
+	}
+	const token = jwt.sign(
+		{
+			userId: user.id,
+		},
+		JWT_SECRET
+	)
+	res.json({ user, token })
 }
