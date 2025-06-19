508763e28e1ec34bc83400a21d151ed0a49e4db9
1quzary <quzaryy@gmail.com>
2025-06-19 12:40:36 +0300

    forgot password

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,15 +1,20 @@
 import { RequestHandler } from 'express'
 import { z } from 'zod'
-import { signupSchema, loginSchema } from '../schema/users'
+import { signupSchema, loginSchema, forgotPasswordSchema } from '../schema/users'
 import { prismaClient } from '../configs/prisma'
 import { hashSync, compareSync } from 'bcrypt'
 import { Ecode } from '../exceptions/root'
 import { brException } from '../exceptions/bad_requests'
 import { UnproccesableEntity } from '../exceptions/validation'
 import { generateToken } from '../utils/jwt'
+import { generateRandomPassword } from '../utils/generatePassword'
+import { Resend } from 'resend'
+
+const resend = new Resend('re_4HhxR6my_JPMqP1DD4brjdKQAE6KNmAdF')
 
 type SignupInput = z.infer<typeof signupSchema>
 type LoginInput = z.infer<typeof loginSchema>
+type forgotPasswordInput = z.infer<typeof forgotPasswordSchema>
 
 export const signup: RequestHandler<unknown, unknown, SignupInput> = async (req, res, next) => {
   signupSchema.parse(req.body)
@@ -41,4 +46,33 @@ export const login: RequestHandler<unknown, unknown, LoginInput> = async (req, r
 
   const token = generateToken({ userId: userWithEmail.id })
   res.json({ user: userWithEmail, token })
+}
+
+export const forgotPassword: RequestHandler<unknown, unknown, forgotPasswordInput> = async (
+  req,
+  res,
+  next
+) => {
+  forgotPasswordSchema.parse(req.body)
+  const { email } = req.body
+
+  const user = await prismaClient.user.findFirst({ where: { email } })
+  if (user) {
+    const newPassword = generateRandomPassword()
+    const hashedPassword = hashSync(newPassword, 10)
+
+    await prismaClient.user.update({
+      where: { id: user.id },
+      data: { password: hashedPassword },
+    })
+
+    await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: 'senyk.yaroslav@student.uzhnu.edu.ua',
+      subject: 'Your new password',
+      html: `<p>Your new password is: <strong>${newPassword}</strong></p>`,
+    })
+  }
+
+  res.json({ message: 'If the email is registered, you will receive a new password.' })
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
-import { signup, login } from '../controllers/auth'
-import { signupSchema, loginSchema } from '../schema/users'
+import { signup, login, forgotPassword } from '../controllers/auth'
+import { signupSchema, loginSchema, forgotPasswordSchema } from '../schema/users'
 import { validateBody } from '../middlewares/validate'
 
 const authRoutes = Router()
 
 authRoutes.post('/signup', validateBody(signupSchema), signup)
 authRoutes.post('/login', validateBody(loginSchema), login)
+authRoutes.post('/forgot-password', validateBody(forgotPasswordSchema), forgotPassword)
 
 export default authRoutes
